041ffbc9401c849da8ecc6a0dc9176b3bd5bec79
James Green <jamesgreen0795@yahoo.co.uk>
2020-02-22 16:47:02 +0000

    added had oil

--- src/store/models/regions.js
+++ src/store/models/regions.js
@@ -11,6 +11,7 @@ const generator = entries => entries.map(entry => {
             openBorders: true,
             flooding: false,
             temperture: 0,
+            hasOil: false,
         },
         ...entry,
     };
