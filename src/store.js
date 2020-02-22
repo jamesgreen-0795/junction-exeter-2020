53b9792dd165e8bc1c17c31efb0ae614397de6a9
David Ackroyd <davidackroydmusic@gmail.com>
2020-02-22 00:38:25 +0000

    added countries

--- src/store.js
+++ src/store.js
@@ -1,18 +1,31 @@
 const uuid = require('uuid/v4');
 
 window.store = {
-    newsfeed: [
-        {
-            uuid: uuid(),
-            message: "Spain has closed its borders",
-        },
-        {
-            uuid: uuid(),
-            message: "Canada has closed its borders",
-        },
-        {
-            uuid: uuid(),
-            message: "France has closed its borders",
-        },
-    ],
+	newsfeed: [],
+	countries: [
+		{
+			uuid: uuid(),
+			timestamp: Date.now(),
+			name: "France",
+			state: {
+				"borders": 0
+			}
+		},
+		{
+			uuid: uuid(),
+			timestamp: Date.now(),
+			name: "Germany",
+			state: {
+				"borders": 0
+			}
+		},
+		{
+			uuid: uuid(),
+			timestamp: Date.now(),
+			name: "The United Kingdom",
+			state: {
+				"borders": 0
+			}
+		},
+	]
 };
