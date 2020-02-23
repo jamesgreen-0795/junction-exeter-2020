b48acb5f887c5cb68a677e3f3582e3c0faf0cc4d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-23 15:00:40 +0000

    Deploy jamesgreen-0795/junction-exeter-2020 to github.com/jamesgreen-0795/junction-exeter-2020.git:gh-pages

--- app.js
+++ app.js
@@ -523,7 +523,7 @@ eval("/**\n * Panzoom for panning and zooming elements using CSS transforms\n * 
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n//\n//\n//\n//\n//\n//\nconst screensContext = __webpack_require__(\"./src/gui/screens sync recursive .vue\");\n\n/* harmony default export */ __webpack_exports__[\"default\"] = ({\n  name: \"layout\",\n  components: { ...screensContext.keys().reduce((screens, contextKey) => {\n      screens[screensContext(contextKey).default.name] = screensContext(contextKey).default;\n      return screens;\n    }, {})\n  }\n});\n\n//# sourceURL=webpack:///./src/gui/layout.vue?./node_modules/babel-loader/lib!./node_modules/vue-loader/lib??vue-loader-options");
+eval("__webpack_require__.r(__webpack_exports__);\n//\n//\n//\n//\nconst screensContext = __webpack_require__(\"./src/gui/screens sync recursive .vue\");\n\n/* harmony default export */ __webpack_exports__[\"default\"] = ({\n  name: \"layout\",\n  components: { ...screensContext.keys().reduce((screens, contextKey) => {\n      screens[screensContext(contextKey).default.name] = screensContext(contextKey).default;\n      return screens;\n    }, {})\n  }\n});\n\n//# sourceURL=webpack:///./src/gui/layout.vue?./node_modules/babel-loader/lib!./node_modules/vue-loader/lib??vue-loader-options");
 
 /***/ }),
 
@@ -956,7 +956,7 @@ eval("var rng = __webpack_require__(/*! ./lib/rng */ \"./node_modules/uuid/lib/r
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"render\", function() { return render; });\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"staticRenderFns\", function() { return staticRenderFns; });\nvar render = function() {\n  var _vm = this\n  var _h = _vm.$createElement\n  var _c = _vm._self._c || _h\n  return _c(\n    \"transition\",\n    { attrs: { name: \"fade\" } },\n    [_c(_vm.$root.store.currentScreen, { tag: \"component\" })],\n    1\n  )\n}\nvar staticRenderFns = []\nrender._withStripped = true\n\n\n\n//# sourceURL=webpack:///./src/gui/layout.vue?./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/vue-loader/lib??vue-loader-options");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"render\", function() { return render; });\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"staticRenderFns\", function() { return staticRenderFns; });\nvar render = function() {\n  var _vm = this\n  var _h = _vm.$createElement\n  var _c = _vm._self._c || _h\n  return _c(_vm.$root.store.currentScreen, { tag: \"component\" })\n}\nvar staticRenderFns = []\nrender._withStripped = true\n\n\n\n//# sourceURL=webpack:///./src/gui/layout.vue?./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/vue-loader/lib??vue-loader-options");
 
 /***/ }),
 
