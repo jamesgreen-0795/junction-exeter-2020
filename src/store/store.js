fd3d443a85a5e22ff9edd9e29eeb237d81bc6cff
JorVLP <47005135+JorVLP@users.noreply.github.com>
2020-02-23 09:36:36 +0000

    update upgrade names

--- src/store/store.js
+++ src/store/store.js
@@ -14,29 +14,29 @@ window.store = {
 		"oil": [
             {
                 uuid: uuid(),
-                name: "Coal Mining",
-                message: "Open new coal mines, increasing the world wide supply of coal.",
+                name: "Fossil Fuels 1",
+                message: "Coal Mining<br>Open new coal mines, increasing the world wide supply of coal.",
                 onPurchase: window.mutations.upgrades.oil1,
                 points: 60
             },
             {
                 uuid: uuid(),
-                name: "Coal Power Plant Research",
-                message: "Discover new, more cost effective ways to cut corners in burning coal.",
+                name: "Fossil Fuels 2",
+                message: "Coal Power Plant Research<br>Discover new, more cost effective ways to cut corners in burning coal.",
                 onPurchase: window.mutations.upgrades.oil2,
                 points: 120
             },
             {
                 uuid: uuid(),
-                name: "Oil Lobbying",
-                message: "Bribe government officials to support the expansion of the oil industry.",
+                name: "Fossil Fuels 3",
+                message: "Oil Lobbying<br>Bribe government officials to support the expansion of the oil industry.",
                 onPurchase: window.mutations.upgrades.oil3,
                 points: 750
             },
             {
                 uuid: uuid(),
-                name: "Artic Oil Reserves",
-                message: "Pass international law to allow the drilling of oil in the artic.",
+                name: "Fossil Fuels 4",
+                message: "Artic Oil Reserves<br>Pass international law to allow the drilling of oil in the artic.",
                 onPurchase: window.mutations.upgrades.oil4,
                 points: 150
             }
@@ -44,29 +44,29 @@ window.store = {
         "transport": [
             {
                 uuid: uuid(),
-                name: "Public Transportation Strike",
-                message: "Bribe Unions to go on stike.",
+                name: "Transportation 1",
+                message: "Public Transportation Strike<br>Bribe Unions to go on stike.",
                 onPurchase: window.mutations.upgrades.transport1,
                 points: 40
             },
             {
                 uuid: uuid(),
-                name: "Oil propelled Transport",
-                message: "Invest in local transport companies, by subsidising new fleets of oil propelled transportation.",
+                name: "Transportation 2",
+                message: "Oil propelled Transport<br>Invest in local transport companies, by subsidising new fleets of oil propelled transportation.",
                 onPurchase: window.mutations.upgrades.transport2,
                 points: 75
             },
             {
                 uuid: uuid(),
-                name: "Fake Emission Tests",
-                message: "Procure a car company and program cars to fake emission standards.",
+                name: "Transportation 3",
+                message: "Fake Emission Tests<br>Procure a car company and program cars to fake emission standards.",
                 onPurchase: window.mutations.upgrades.transport3,
                 points: 300
             },
             {
                 uuid: uuid(),
-                name: "Cheaper Jet Fuel",
-                message: "Get more people flying by developing a new, cheaper airplane.\
+                name: "Transportation 4",
+                message: "Cheaper Jet Fuel<br>Get more people flying by developing a new, cheaper airplane.\
                         increasing competition, driving down the cost to fly",
                 onPurchase: window.mutations.upgrades.transport4,
                 points: 600
@@ -75,29 +75,29 @@ window.store = {
         "fake": [
             {
                 uuid: uuid(),
-                name: "Veganism cuts your life expectancy in half.",
-                message: "Title of the latest issue of The Daily Fact.",
+                name: "Disinformation 1",
+                message: "Veganism cuts your life expectancy in half.<br>Title of the latest issue of The Daily Fact.",
                 onPurchase: window.mutations.upgrades.fake1,
                 points: 7
             },
             {
                 uuid: uuid(),
-                name: "Alien abductions NUMBER ONE cause of climate change.",
-                message: "Clickbait headline on thedailybuzz.co.uk.",
+                name: "Disinformation 2",
+                message: "Alien abductions NUMBER ONE cause of climate change.<br>Clickbait headline on thedailybuzz.co.uk.",
                 onPurchase: window.mutations.upgrades.fake2,
                 points: 30
             },
             {
                 uuid: uuid(),
-                name: "Micro Plastics found to be nuturient rich food source for fish.",
-                message: "Viral social media post on the book of faces.",
+                name: "Disinformation 3",
+                message: "Micro Plastics found to be nuturient rich food source for fish.<br>Viral social media post on the book of faces.",
                 onPurchase: window.mutations.upgrades.fake3,
                 points: 100
             },
             {
                 uuid: uuid(),
-                name: "Just throw your rubbish in the ocean, it'll end up there anyways.",
-                message: "Off handed comment by the head of the UN.",
+                name: "Disinformation 4",
+                message: "Just throw your rubbish in the ocean, it'll end up there anyways.<br>Off handed comment by the head of the UN.",
                 onPurchase: window.mutations.upgrades.fake4,
                 points: 175
             }
@@ -105,29 +105,29 @@ window.store = {
         "beef": [
             {
                 uuid: uuid(),
-                name: "Fast Food",
-                message: "Start a new meat friendly fast food",
+                name: "Agriculture 1",
+                message: "Fast Food<br>Start a new meat friendly fast food",
                 onPurchase: window.mutations.upgrades.beef1,
                 points: 15
             },
             {
                 uuid: uuid(),
-                name: "Fishing Trawlers",
-                message: "Increase the number of ships trawling the ocean for fish.",
+                name: "Agriculture 2",
+                message: "Fishing Trawlers<br>Increase the number of ships trawling the ocean for fish.",
                 onPurchase: window.mutations.upgrades.beef2,
                 points: 225
             },
             {
                 uuid: uuid(),
-                name: "Buring of the Amazon",
-                message: "Burn down parts of the amazon to allow more room for cattle.",
+                name: "Agriculture 3",
+                message: "Buring of the Amazon<br>Burn down parts of the amazon to allow more room for cattle.",
                 onPurchase: window.mutations.upgrades.beef3,
                 points: 450
             },
             {
                 uuid: uuid(),
-                name: "Vegetarianism is Sin",
-                message: "World religious leader receives a message from a higher power stating that not eating meat everyday is a sin.",
+                name: "Agriculture 4",
+                message: "Vegetarianism is Sin<br>World religious leader receives a message from a higher power stating that not eating meat everyday is a sin.",
                 onPurchase: window.mutations.upgrades.beef4,
                 points: 1000
             }
